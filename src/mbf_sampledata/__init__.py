6eea0ee62e86f0e8a205b2e93771548dd532be70
finkernagel <finkernagel@imt.uni-marburg.de>
2019-04-10 09:05:27 +0000

    added get_sample_path wich returns a Path

--- src/mbf_sampledata/__init__.py
+++ src/mbf_sampledata/__init__.py
@@ -4,3 +4,8 @@ from pathlib import Path
 def get_sample_data(fn):
     here = Path(__file__).parent
     return str((here / 'data' / fn).absolute())
+
+def get_sample_path(fn):
+    here = Path(__file__).parent
+    return (here / 'data' / fn).absolute()
+
